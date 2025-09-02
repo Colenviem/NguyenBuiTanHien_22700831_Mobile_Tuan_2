ec4d9f135eb3ae84321b964dc677f0789dd83f00
Colenviem <hiennguyenbuitan@gmail.com>
2025-09-02 08:53:29 +0700

    17. Use for await...of to iterate over an array of Promises.

---
+++ dist/exercise_17.js
@@ -0,0 +1,21 @@
+"use strict";
+// 17. Use for await...of to iterate over an array of Promises.
+const delayAndReturn = (num, delay) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(num);
+        }, delay);
+    });
+};
+const run17 = async () => {
+    const promises = [
+        delayAndReturn(1, 1000),
+        delayAndReturn(2, 2000),
+        delayAndReturn(3, 3000)
+    ];
+    for await (const result of promises) {
+        console.log("Waiting for promise to resolve...");
+        console.log(result);
+    }
+};
+run17();
