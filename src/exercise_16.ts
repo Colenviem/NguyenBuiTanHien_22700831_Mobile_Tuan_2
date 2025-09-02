6d8b1ec306604fb4dbfe71f44a815cc568c21372
Colenviem <hiennguyenbuitan@gmail.com>
2025-09-02 08:51:08 +0700

    16. Call multiple async functions in parallel using Promise.all().

---
+++ src/exercise_16.ts
@@ -0,0 +1,27 @@
+// 16. Call multiple async functions in parallel using Promise.all().
+const multiplyAfterDelay3 = (num: number): Promise<number> => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(num * 3);
+        }, 1000);
+    });
+}
+
+const multiplyAfterDelay4 = (num: number): Promise<number> => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(num * 2);
+        }, 1000);
+    });
+}
+
+const run16 = async () => {
+    const [result1, result2] = await Promise.all([
+        multiplyAfterDelay3(5),
+        multiplyAfterDelay4(5)
+    ]);
+    console.log(result1);
+    console.log(result2);
+}
+
+run16();
