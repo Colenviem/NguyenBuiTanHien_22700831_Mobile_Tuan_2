7c64c4b0d6265446c50ca316c5a1ec3b846f7f82
Colenviem <hiennguyenbuitan@gmail.com>
2025-09-02 10:12:13 +0700

    29. Write an async function queueProcess() that processes tasks sequentially in a queue.

--- dist/exercise_29.js
+++ dist/exercise_29.js
@@ -1,12 +1,12 @@
+"use strict";
 // 29. Write an async function queueProcess() that processes tasks sequentially in a queue.
-const simulateTask29 = (id: number, time: number): Promise<string> => {
+const simulateTask29 = (id, time) => {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve(`Task ${id} done ${time} ms`);
         }, time);
     });
-}
-
+};
 const queueProcess = async () => {
     const tasks = [
         { id: 1, time: 1000 },
@@ -15,19 +15,14 @@ const queueProcess = async () => {
         { id: 4, time: 4000 },
         { id: 5, time: 5000 }
     ];
-
     console.log('Starting task processing in queue...');
-    
     for (const task of tasks) {
         const result = await simulateTask29(task.id, task.time);
         console.log(result);
-    }   
-
+    }
     console.log('All tasks completed');
-}
-
+};
 const run29 = async () => {
     await queueProcess();
-}
-
+};
 run29();
